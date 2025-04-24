631b0c72d46d008c98d5b1e48957f8b49ed5aff0
unknown <sufyancsk92@gmail.com>
2025-04-24 16:12:58 +0530

    Fix dependencies and build script for Render

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cheerio = require("cheerio");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
